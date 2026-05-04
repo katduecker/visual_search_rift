a3226cb8053b3643af0d18752e838466fdd2e228
Katharina Duecker (Psychology) <dueckerk@bear-pg-login06.bear.cluster>
2026-05-04 18:35:43 +0100

    add comments, update fourier

--- matlab_scripts/alpha/a_tfr_fourier.m
+++ matlab_scripts/alpha/a_tfr_fourier.m
@@ -4,47 +4,29 @@
 % a. TFR of alpha over all trials
 
 % [c] Katharina Duecker, katharina.duecker@gmail.com
-% last changed/checked 2 Aug 2023
+% last changed/checked 4 May 2026
 
-% Inputs
-% - s: subject index
-% - c_idx: condition index (it's just 1 because we are looking at all data)
-% - toi: start and end time point the sliding window is centered over
-% - winl: window length
-% Output
-% - TFR of power with window length 1 sec, Hanning window, moved in 50 sec
-% steps
-
-% [c] K. Duecker, PhD candidate Neuronal Oscillations group
-% last changed: 23/03/2022
-% katharina.duecker@gmail.com
-
-%% Alpha analysis pipeline
-% a. TFR of alpha power over all trials (keep trials) & average trials
-% b. Identify IAF and SOI
-% c. align TFR to IAF
-% d. contrast conditions
-% e. control analysis: compare alpha for fast vs slow
 
 function a_tfr_fourier(s)
 
+% init spectrogram 
 toi = -1.75:0.05:0.5;
-winl = 1;
+winl = 0.5;
 %% settings
-pth = '/rds/projects/j/jenseno-visual-search-rft/Visual Search RFT';
+pth = '/rds/projects/j/jenseno-visual-search-rft/visual_search_rift';
 
 inpth = fullfile(pth,'results','meg','4 split conditions','sinusoid','clean data all trials');
 outpth = fullfile(pth,'results','meg','6 Alpha', 'pow');
 
 rmpath('/rds/projects/2018/jenseno-entrainment/fieldtrip')
-addpath('/rds/projects/j/jenseno-visual-search-rft/fieldtrip')
+addpath('/rds/projects/j/jenseno-visual-search-rft/visual_search_rift/fieldtrip')
 
-load(fullfile(pth,'matlab scripts/',"preprocessing MEG/",'idx_subjoi.mat'));
+load(fullfile(pth,'matlab_scripts/',"preproc_meg/",'idx_subjoi.mat'));
 
 outpth = fullfile(outpth,subj{s});
 mkdir(outpth)
 
-% list condition files that contain 'condi'
+%% list all files
 d = dir(fullfile(inpth,subj{s}));
 
 d = {d.name};
@@ -53,7 +35,7 @@ files = d(end);
 
 load(fullfile(inpth,subj{s},files{1}));
 
-%% TFR
+%% TFR over all trials
 cfg = [];
 cfg.method = 'mtmconvol';
 cfg.channel = 'MEGGRAD';
@@ -63,6 +45,7 @@ cfg.t_ftimwin = ones(length(cfg.foi),1)*winl;
 cfg.toi = toi;
 cfg.keeptrials = 'yes';
 cfg.output = 'fourier';
+cfg.pad = 'nextpow2';
 TFR = ft_freqanalysis(cfg,data);
 
 TFR.powspctrm = abs(TFR.fourierspctrm);
--- matlab_scripts/alpha/e1_alpha_fast_slow_balanced.m
+++ matlab_scripts/alpha/e1_alpha_fast_slow_balanced.m
@@ -1,10 +1,8 @@
 %% Visual Search + RIFT
-% Duecker, Shapiro, Hanslmayr, Wolfe, Pan, and Jensen
-
 % e1. calculate TFRs for fast vs slow trials (Fig. 4 d)
 
 % [c] Katharina Duecker, katharina.duecker@gmail.com
-% last changed/checked 3 May 2026
+% last changed/checked 4 May 2026
 
 % Inputs
 % - s: subject index
@@ -12,14 +10,18 @@
 % - split_ta_tp (bool): split into target absent/present?
 
 % Output
-% - soi_grad: gradiometers with high alpha power
-% iaf_grad: identified IAF in gradiometers
+% - specfast: spectrum for fast trials
+% - specslow: spectrum slow trials
+% - freqvec: frequency vector for plotting (same for all P's)
 
 
 function e1_alpha_fast_slow_balanced(s)
 
-num_bins = 4;
-winl = 0.5;
+num_bins = 4;           % number of bins for balanced splot (over time)
+winl = 0.5;             % window length for spectrograms
+
+% conditions
+% ni/ti: unguided/guided, 16/32: set sizes, ta/tp: target present/absent
 condi_all = {{'ni','16ta'},{'ti','16ta'}, {'ni','32ta'},{'ti','32ta'},{'ni','16tp'},{'ti','16tp'}, {'ni','32tp'},{'ti','32tp'}};
 
 
@@ -31,27 +33,32 @@ outpth = fullfile(pth,'results','meg','6 Alpha','rt_balanced');
 glmpth = fullfile(pth,'results','meg','9 GLM', 'glm_spec');
 addpath('/rds/projects/j/jenseno-visual-search-rft/visual_search_rift/fieldtrip')
 
+% load pre-processed subject IDs
 load(fullfile(pth,'matlab_scripts/',"preproc_meg",'idx_subjoi.mat'));
 
-load(fullfile(glmpth, 'glm_RT_soi_iaf_subj'))
-
+% load GLM results (SOI, sign time points)
+load(fullfile(glmpth, 'glm_RT_soi_iaf_subj'), 'subj_soi', 'glm_time_sig')
 soi = subj_soi{s};
 time_oi = glm_time_sig;
 
 %% load data
 
+
+% list all data files
 d = dir(fullfile(inpth,subj{s}));
 files = {d.name};
 files(1:2) = [];
 
+% init variables
+freqvec = 4:1/winl:30;          % frequency vector
 
-freqvec = 4:1/winl:30;
+% empty fast vs slow spectra for each condition
 spec_fast = zeros(length(condi_all), length(freqvec));
 spec_slow = spec_fast;
 
 for c_idx = 1:length(condi_all)
+    % find all files that belong to current conditions
     condi = condi_all{c_idx};
-    % find relevant data files
     condi_files = zeros(length(files),1);
     for c = 1:length(condi)
         condi_files = condi_files + cell2mat(cellfun(@(x) ~isempty(x),regexp(files,condi{c}),'UniformOutput',false))';
@@ -60,22 +67,19 @@ for c_idx = 1:length(condi_all)
 
     c_files = files(condi_files);
 
+    % load first file and append remaining trials
     load(fullfile(inpth,subj{s},c_files{1}));
     trl_idx = find(trlcur);
-
-    % performance in first trials
     trl_perf = perf_cur;
-
-    % load data
     data_load = data_trig;
-    % data_load = rmfield(data_load,'sampleinfo');
-    % load & append data
+
+    % append data
     for f = 2:length(c_files)
         clear data_trig trlcur perf_cur
         load(fullfile(inpth,subj{s},c_files{f}));
         % append data
         data_load = ft_appenddata([],data_load,data_trig);
-        %append performance
+        % append performance
         trl_perf = [trl_perf;perf_cur];
     end
 
@@ -84,18 +88,18 @@ for c_idx = 1:length(condi_all)
     clear data_load
 
     %% Balanced split
-    rt_all = [trl_perf{:,3}];
+    rt_all = [trl_perf{:,3}];            % reaction time all trials
 
-    lblock = floor(length(rt_all)/num_bins);
+    lblock = floor(length(rt_all)/num_bins);    % length on 1 block (bin)
 
-    % sort according to t-o-t
-    [~,I] = sort([trl_perf{:,end}]);
+    % sort trials according to t-o-t
+    [~,I] = sort([trl_perf{:,end}]);            
 
     % sort RT
     rt_all = rt_all(I);
 
+    % bin reaction time and trials based on TOT
     h = 1;
-
     rt_block = zeros(num_bins,lblock);
     idx = zeros(num_bins,lblock);
     for b = 1:num_bins
@@ -108,6 +112,7 @@ for c_idx = 1:length(condi_all)
 
     end
 
+    % get fast and slow trials over bins
     idx_fast = [];
     idx_slow = [];
     for b = 1:num_bins
@@ -130,8 +135,9 @@ for c_idx = 1:length(condi_all)
     clear data
 
 
-    %% Power 
+    %% TFR
 
+    % spectrogram of fast and slow trials
     cfg = [];
     cfg.method = 'mtmconvol';
     cfg.channels = {'MEGGRAD'};
@@ -140,12 +146,14 @@ for c_idx = 1:length(condi_all)
     cfg.t_ftimwin = ones(length(cfg.foi),1)*winl;
     cfg.toi = -1.75:0.05:1;
     cfg.keeptrials = 'no';
+    cfg.pad = 'nextpow2';
     TFRfast = ft_freqanalysis(cfg,data_fast);
     TFRslow = ft_freqanalysis(cfg,data_slow);
     
     TFRfast = ft_combineplanar([], TFRfast);
     TFRslow = ft_combineplanar([], TFRslow);
 
+    % average over time points and channels of interest
     cfg = [];
     cfg.avgovertime = 'yes';
     cfg.latency = time_oi;
@@ -154,11 +162,12 @@ for c_idx = 1:length(condi_all)
     IAFfast = ft_selectdata(cfg,TFRfast);
     IAFslow = ft_selectdata(cfg,TFRslow);
 
+    % save spectra
     spec_slow(c_idx,:) = IAFslow.powspctrm./max(IAFslow.powspctrm);
     spec_fast(c_idx,:) = IAFfast.powspctrm./max(IAFslow.powspctrm);
     
 
-clear IAFfast IAFslow idx_fast idx_slow data_fast data_slow I rt_block idx trl_perf
+    clear IAFfast IAFslow idx_fast idx_slow data_fast data_slow I rt_block idx trl_perf
 end
 save(fullfile(outpth,subj{s},'spectra_RTsplit.mat'),'spec_fast', 'spec_slow', 'freqvec')
 
